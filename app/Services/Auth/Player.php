09c66c98e44be9a6919a1bb904298957fe34363d
blue rain <dahshin2012@gmail.com>
2024-04-09 13:47:02 +0800

    [awei][test-loginPlayerSuccess]

--- app/Services/Auth/Player.php
+++ app/Services/Auth/Player.php
@@ -2,16 +2,56 @@
 
 namespace App\Services\Auth;
 
+use App\Repositories\Mydb\Player as PlayerRepositories;
+use App\Repositories\RedisRepository as RedisRepositories;
 use Illuminate\Support\Facades\Response;
 
 class Player
 {
+    private $oPlayerRepositories;
+    private $oRedisRepositories;
+
+    public function __construct(PlayerRepositories $_oPlayerRepositories, RedisRepositories $_oRedisRepositories)
+    {
+        $this->oPlayerRepositories = $_oPlayerRepositories;
+        $this->oRedisRepositories = $_oRedisRepositories;
+    }
+
     public function registerResult($_bCreate)
     {
         if ($_bCreate) {
             return Response::json(['success' => true, 'message' => '註冊成功'], 200);
         } else {
             return Response::json(['success' => false, 'message' => '註冊失敗'], 422);
+        }
+    }
+
+    public function loginPlayer($_aData)
+    {
+        $aPlayer = $this->oPlayerRepositories->findMemberByEmail($_aData['email']);
+
+        if ($aPlayer != null) {
+            if (password_verify($_aData['password'], $aPlayer['password'])) {
+                $sUID = substr(sha1(uniqid('', true)), 1, 10);
+                $sUID2 = substr(md5(uniqid('', true)), 3, 12);
+
+                $this->oRedisRepositories->storeUIDs($aPlayer['id'], [$sUID, $sUID2]);
+
+                return [
+                    'state' => true,
+                    'message' => '登入成功!',
+                ];
+            } else {
+                return [
+                    'state' => false,
+                    'message' => '密碼錯誤!',
+                ];
+            }
+        } else {
+            [
+                'state' => false,
+                'message' => '帳號不存在!',
+            ];
         }
     }
 }
---
+++ tests/Unit/Services/Auth/PlayerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit\Services\Auth;
+
+use App\Repositories\Mydb\Player as PlayerReositories;
+use App\Repositories\RedisRepository as RedisRepositories;
+use App\Services\Auth\Player as PlayerServices;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class PlayerTest extends TestCase
+{
+    private $oPlayerService;
+    private $oRedisRepository;
+    private $oPlayerRepository;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->oRedisRepository = Mockery::mock(RedisRepositories::class);
+
+        $this->oPlayerRepository = Mockery::mock(PlayerReositories::class);
+
+        $this->oPlayerService = new PlayerServices($this->oPlayerRepository, $this->oRedisRepository);
+    }
+
+    /**
+     * testLoginPlayerSuccess
+     *
+     * @group player
+     * @return void
+     */
+    public function testLoginPlayerSuccess()
+    {
+        $aPlayerData = [
+            'id' => 1,
+            'email' => 'test@example.com',
+            'password' => password_hash('12345678', PASSWORD_DEFAULT),
+        ];
+
+        $this->oPlayerRepository->shouldReceive('findMemberByEmail')->with('test@example.com')->andReturn($aPlayerData);
+
+        $this->oRedisRepository->shouldReceive('storeUIDs')->once()->with($aPlayerData['id'], Mockery::type('array'));
+        
+        $aResult = $this->oPlayerService->loginPlayer([
+            'email' => $aPlayerData['email'],
+            'password' => '12345678',
+        ]);
+
+        $this->assertTrue($aResult['state']);
+        $this->assertEquals('登入成功!', $aResult['message']);
+    }
+}
