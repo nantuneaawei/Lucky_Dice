d5ebb6e5a0777372763830822e1f02af3fb5fd92
blue rain <dahshin2012@gmail.com>
2024-04-10 17:49:00 +0800

    [awei][add cookieService]

---
+++ app/Services/CookieService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Cookie;
+
+class CookieService
+{
+    public function put($_sKey, $_sValue, $_iMinutes = 60)
+    {
+        Cookie::queue($_sKey, $_sValue, $_iMinutes);
+    }
+
+    public function get($_sKey, $_sDefault = null)
+    {
+        return Cookie::get($_sKey, $_sDefault);
+    }
+
+    public function forget($_sKey)
+    {
+        Cookie::queue(Cookie::forget($_sKey));
+    }
+}
